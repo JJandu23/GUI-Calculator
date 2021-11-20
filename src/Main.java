684ebc38d497d385fc702bc84a0b911c467ec9aa
Jashanpreet Jandu <67085053+Jashaja18@users.noreply.github.com>
2021-11-19 22:44:15 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,13 @@
+package com.company;
 
+import com.company.view.Calculator;
+
+public class Main {
+
+    // Main method
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.start();
+        calculator.setLocationRelativeTo(null);
+    }
+}
